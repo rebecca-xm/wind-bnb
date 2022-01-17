227654b9491f341a16c3e843c5291d727f6ab96e
Floriana Domianello <floriana.domianello@gmail.com>
2022-01-17 23:08:45 +0100

    fix:style css searchbar and test console.log input searchbar

--- src/Components/Searchbar/index.jsx
+++ src/Components/Searchbar/index.jsx
@@ -1,12 +1,37 @@
 import { data } from "../data/stays";
 import { useState } from "react";
 import style from "./Searchbar.module.scss";
+// importata libreria npm install @heroicons/react
+import {SearchIcon} from "@heroicons/react/solid"
+// importata libreria npm install --save react-date-range
+// import "react-date-range/dist/styles.css"; // style css
+// import "react-date-range/dist/theme/default.css"; // theme css
+// import {DateRangePicker} from "react-date-range";
 
 function Search() {
   const [searchTerm, setSearchTerm] = useState("");
+  console.log(searchTerm);
+
+  //calendario inzio e fine soggiorno 
+  // const [startDate, setstartDate] = useState(new Date());
+  // const [endDate, setendDate] = useState(new Date());
+  
+  // const handSelect =(ranges) => {
+  //   setstartDate(ranges.selection.startDate)
+  //   setendDate(ranges.selection.endDate)
+  // }
+
+  // const selectionRange = {
+  //   startDate:startDate,
+  //   endDate:endDate,
+  //   key:"Selection",
+  // }
+
+ 
   return (
     <div className={style.Searchbar}>
-      <input
+      <input className={style.SearchbarInput}
+      value={searchTerm}
         type="search"
         placeholder="Search..."
         onChange={(event) => {
@@ -23,10 +48,22 @@ function Search() {
           }
         })
         .map((val, key) => {
-          <div className={style.Search} key={key}>
+          <div className={style.Search} key={key} >
             <p>{val.city} </p>
           </div>
         })}
+    <SearchIcon className={style.searchIcon}/>
+    
+    {/* {searchTerm && (
+      <div>
+      <DateRangePicker 
+      ranges={[selectionRange]}
+      minDate={new Date()}
+      rangeColors={["#FD5B61"]}
+      onChange={handSelect}
+    />
+    </div>
+    )} */}
     </div>
   );
 }
