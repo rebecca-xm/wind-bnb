e95cfc3c3449eeb1de38b11aada409513e5c5e06
MarcoM94 <voldrak94@gmail.com>
2022-01-22 14:06:31 +0100

    fix: select

--- src/Components/Searchbar/index.jsx
+++ src/Components/Searchbar/index.jsx
@@ -24,10 +24,15 @@ const Search = ({ cercaInput, handleCercaInput}) =>{
       >
 
       <option value="">Tutti</option> 
-      {data.rooms.filter((flats => flats.city))
+      {/* {data.rooms.filter((flats => flats.city))
         .map((flats)=> (
       <option value={flats.city}>`{flats.city}, Finland`</option> 
-        ))}
+        ))} */}
+        <option value="Helsinki">Helsinki, Finland</option> 
+        <option value="Turku">Turku, Finland</option> 
+        <option value="Vaasa">Vaasa, Finland</option> 
+        <option value="Oulu">Oulu, Finland</option> 
+
       </select>
       </form>
     </div>
