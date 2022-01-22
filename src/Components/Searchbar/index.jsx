c27eb44079cfa8a92d73f23759d48b32409a4150
MarcoM94 <voldrak94@gmail.com>
2022-01-22 13:18:12 +0100

    feat: add select bar

--- src/Components/Searchbar/index.jsx
+++ src/Components/Searchbar/index.jsx
@@ -1,4 +1,4 @@
-
+import { data } from './../data/stays';
 import style from "./Searchbar.module.scss";
 
 
@@ -12,15 +12,24 @@ const Search = ({ cercaInput, handleCercaInput}) =>{
  
   return (
     <div>
-      <p>Cerca:</p>
-      <input
+    <form>
+      <label>Cerca:</label>
+      <select
         value={cercaInput}
         onChange={handleCercaInput}
         type="text"
         name="cerca"
         id="cerca"
         placeholder="Search..."
-      />
+      >
+
+      <option value="">Tutti</option> 
+      {data.rooms.filter((flats => flats.city))
+        .map((flats)=> (
+      <option value={flats.city}>`{flats.city}, Finland`</option> 
+        ))}
+      </select>
+      </form>
     </div>
   
   );
