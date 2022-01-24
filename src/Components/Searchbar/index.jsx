97d02abc52650a6292268da5f19c53646679303e
Stefano <io.stefanorusso@gmail.com>
2022-01-24 19:22:39 +0100

    feat: add counter

--- src/Components/Searchbar/index.jsx
+++ src/Components/Searchbar/index.jsx
@@ -1,49 +1,43 @@
-import { useState,useEffect, setState } from 'react';
-import { data } from './../data/stays';
+import { useState, useEffect, setState } from "react";
+import { data } from "./../data/stays";
 import style from "./Searchbar.module.scss";
 
+const Search = ({ cercaInput, handleCercaInput, onClickNav }) => {
+  const buttonsName = data.map(({ city, country }) => ({
+    city,
+    country,
+  }));
+  const uniq = [...new Set(buttonsName)];
 
-
-
-
-const Search = ({ cercaInput, handleCercaInput}) =>{
-  
-const [cityInp, setCityInp] = useState("");
-
-// const cityInpi = `${cityInp}` + ", Finland" || null;
-  
-  useEffect(() => { setCityInp() },[])
-
- 
   return (
     <div className={style.Searchbar}>
-    <form className={style.formSearch}>
-    <div className={style.locInp}>
-      <label>Location:</label> <br/>
-      <input
-        value={cityInp}
-        onChange={handleCercaInput}
-        type="text"
-        name="cerca"
-        id="cerca"
-        className={style.searchbarInput}
-        placeholder="Search..."
-      />
-</div>
-      {data.rooms.filter((flats => flats.city))
-        .map((flats)=> (
-      <input className={style.btnCity} type="button" onClick={(e) => setCityInp(`${flats.city}`)} value={flats.city}/>
+      <form className={style.formSearch}>
+        <div className={style.locInp}>
+          <label>Location:</label> <br />
+          <input
+            value={cercaInput}
+            onChange={handleCercaInput}
+            type="text"
+            name="cerca"
+            id="cerca"
+            className={style.searchbarInput}
+            placeholder="Search..."
+          />
+        </div>
+        {uniq.map((element) => (
+          <input
+            className={style.btnCity}
+            type="button"
+            onClick={() => onClickNav(element.city)}
+            value={element.city}
+          />
         ))}
         {/* <input onClick={() => setCityInp("Helsinki")} value="Helsinki"/> 
         <input onClick={() => setCityInp("Turku")} value="Turku"/>
         <input onClick={() => setCityInp("Vaasa")} value="Vaasa"/>
         <input onClick={() => setCityInp("Oulu")} value="Oulu"/> */}
-
-      
-
       </form>
     </div>
-  
   );
-}
+};
 export default Search;
--- src/Components/Stays/index.jsx
+++ src/Components/Stays/index.jsx
@@ -1,16 +1,13 @@
 import Styled from "./stays.module.scss";
-import { data } from './../data/stays';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-const Stays = () => {
-  
-    return (
-        <>
-        <h1>Stays in Finland</h1>
-        <h3 key={data.rooms.lenght()}> + {...room.lenght()+1} stays </h3>  
-        
-        </>
-    )
-}
+const Stays = ({ count }) => {
+  return (
+    <>
+      <h1>Stays in Finland</h1>
+      <h3> stays {count} </h3>
+    </>
+  );
+};
 
 export default Stays;
