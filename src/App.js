668bafa777879ca9e4d18e6b632ee79905e25fcf
MarcoM94 <voldrak94@gmail.com>
2022-01-27 20:23:20 +0100

    fix: general conflict

--- src/App.js
+++ src/App.js
@@ -1,55 +1,19 @@
-import styles from "./App.module.scss";
-import Header from "./Components/Header/index.jsx";
-import FlatsCard from "./Components/FlatsCard";
+import styles from './App.module.scss';
+import Header from './Components/Header/index.jsx';
+import FlatsCard from './Components/FlatsCard';
 import Search from "./Components/Searchbar/index";
-import Stays from "./Components/Stays/index";
 
-import { data } from "./Components/data/stays";
-import { useState, useEffect } from "react";
+import { data } from './Components/data/stays';
+import { useState } from 'react';
 
-// const  [open, setOpen] =  useState(false)
-// const modalClassName = `modal ${ open ? 'modal-open' : ''}`
-// <div className={modalClassName} > {.....roba modal} </div>
 
 function App() {
-  const [cercaInput, setCercaInput] = useState("");
-  const [rooms, setRooms] = useState(data);
-
-  // const handleCercaInput = (e) => setCercaInput(e.target.value);
-
-  // useEffect(() => {
-  //   const search = data.filter(
-  //     (flats) =>
-  //       flats.city.toLowerCase().includes(cercaInput.toLowerCase()) ||
-  //       cercaInput === ""
-  //   );
-
-  //   setRooms(search);
-  // }, [cercaInput]);
+  
 
   return (
     <div className="App">
       <Header />
-      {/* <Search
-        cercaInput={cercaInput}
-        handleCercaInput={handleCercaInput}
-        onClickNav={setCercaInput}
-      />  */}
-      {/* <Stays count={rooms.length} />
-      <section className={styles.cardContainer}>
-        {rooms.map((flats) => (
-          <FlatsCard
-            key={flats.id}
-            image={flats.photo}
-            superHost={flats.superHost}
-            type={flats.type}
-            beds={flats.beds}
-            rating={flats.rating}
-            title={flats.title}
-          />
-        ))}
-      </section>
-      <Stays count={rooms.length} /> */}
+      
     </div>
   );
 }
--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,86 +1,82 @@
-// import logo from "./../Img/logo.png";
-// import styles from "./Header.module.scss";
-// import Search from "../Searchbar";
-// import { data } from "../data/stays";
-// import {SearchIcon} from "@heroicons/react/solid";
-// import { useState, useEffect } from "react";
-// import Stays from "../Stays";
-// import FlatsCard from "../FlatsCard";
+import logo from "./../Img/logo.png";
+import styles from "./Header.module.scss";
+import Search from "../Searchbar";
+import { data } from "../data/stays";
+import {SearchIcon} from "@heroicons/react/solid";
+import { useState, useEffect } from "react";
+import Stays from "../Stays";
+import FlatsCard from "../FlatsCard";
 
 
-// const Header = () => {
-//    const [cercaInput, setCercaInput] = useState("");
-//    const [rooms, setRooms] = useState(data);
-//   const handleCercaInput = (e) => setCercaInput(e.target.value);
+const Header = () => {
+   const [cercaInput, setCercaInput] = useState("");
+   const [rooms, setRooms] = useState(data);
+  const handleCercaInput = (e) => setCercaInput(e.target.value);
 
-//   const [isModalShown, setModalShown] = useState(false);
+  const [isModalShown, setModalShown] = useState(false);
 
-//   const handleModal = () => {
-//     setModalShown(true);
-//   }
+  const handleModal = () => {
+    setModalShown(true);
+  }
 
-//   useEffect(() => {
-//     const search = data.filter(
-//       (flats) =>
-//         flats.city.toLowerCase().includes(cercaInput.toLowerCase()) ||
-//         cercaInput === ""
-//     );
+  useEffect(() => {
+    const search = data.filter(
+      (flats) =>
+        flats.city.toLowerCase().includes(cercaInput.toLowerCase()) ||
+        cercaInput === ""
+    );
 
-//     setRooms(search);
-//   }, [cercaInput]);
+    setRooms(search);
+  }, [cercaInput]);
 
 
-//   return (
-//     <>
-//     <div className={styles.header}>
-//       <img
-//         src={logo}
-//         alt="logo"
-//         width="150px"
-//       />
-//      <div className={styles.Searchbar}>
-//       <input className={styles.SearchbarInput}
-//         type="search"
-//         placeholder="Search..."
-//         onClick={handleModal}
-//       />   
-//        <input className={styles.SearchbarInput}
-//         type="search"
-//         placeholder="Search..."
-//         onClick={handleModal}
-//       />  
-// <<<<<<< Modale
-//         {isModalShown && <Search
-//         cercaInput={cercaInput}
-//         handleCercaInput={handleCercaInput}
-//         onClickNav={setCercaInput}
+  return (
+    <>
+    <div className={styles.header}>
+      <img
+        src={logo}
+        alt="logo"
+        width="150px"
+      />
+     <div className={styles.Searchbar}>
+      <input className={styles.SearchbarInput}
+        type="search"
+        placeholder="Search..."
+        onClick={handleModal}
+      />   
+       <input className={styles.SearchbarInput}
+        type="search"
+        placeholder="Search..."
+        onClick={handleModal}
+      />  
+        {isModalShown && <Search
+        cercaInput={cercaInput}
+        handleCercaInput={handleCercaInput}
+        onClickNav={setCercaInput}
       
-//       /> }
-//       <SearchIcon className={styles.searchIcon}
-//       onClick={handleModal}
-//       />
-// =======
-//       <SearchIcon className={styles.searchIcon}/>
-// >>>>>>> main
-//     </div> 
-//     </div>
-//        <Stays count={rooms.length} />
-//        <section className={styles.cardContainer}>
-//          {rooms.map((flats) => (
-//            <FlatsCard
-//              key={flats.id}
-//              image={flats.photo}
-//              superHost={flats.superHost}
-//              type={flats.type}
-//              beds={flats.beds}
-//              rating={flats.rating}
-//              title={flats.title}
-//            />
-//          ))}
-//        </section>
-//        <Stays count={rooms.length} />
-//        </>
-//   );
-// };
+      /> }
+      <SearchIcon className={styles.searchIcon}
+      onClick={handleModal}
+      />
+    </div> 
+    </div>
+       <Stays count={rooms.length} />
+       <section className={styles.cardContainer}>
+         {rooms.map((flats) => (
+           <FlatsCard
+             key={flats.id}
+             image={flats.photo}
+             superHost={flats.superHost}
+             type={flats.type}
+             beds={flats.beds}
+             rating={flats.rating}
+             title={flats.title}
+           />
+         ))}
+       </section>
+       <Stays count={rooms.length} />
+       </>
+  );
+};
 
-// export default Header;
+export default Header;
