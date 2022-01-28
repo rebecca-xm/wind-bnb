fb73f72d9dc74282c2128c661d46c7b8f0a36a6a
Fablizio <gats200@hotmail.it>
2022-01-28 17:51:41 +0100

    feat-close-modal

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,35 +1,28 @@
 import logo from "./../Img/logo.png";
 import styles from "./Header.module.scss";
-import {SearchIcon} from "@heroicons/react/solid"
+import { SearchIcon } from "@heroicons/react/solid";
 
-const Header = ({ onClickMod }) =>  {
-
-
+const Header = ({ onClickMod }) => {
   return (
     <div className={styles.header}>
-      <img
-        src={logo}
-        alt="logo"
-        width="150px"
-      />
-     <div className={styles.Searchbar}>
-      <input className={styles.SearchbarInput}
-        type="search"
-        placeholder="Search..."
-        onClick= {onClickMod}
-      />   
-       <input className={styles.SearchbarInput}
-        type="search"
-        placeholder="Search..."
-        onClick= {onClickMod}
-      />  
-      <SearchIcon className={styles.searchIcon}
-        onClick= {onClickMod}
-      />
-    </div> 
+      <img src={logo} alt="logo" width="150px" />
+      <div className={styles.Searchbar}>
+        <input
+          className={styles.SearchbarInput}
+          type="search"
+          placeholder="Location"
+          onClick={onClickMod}
+        />
+        <input
+          className={styles.SearchbarInput}
+          type="search"
+          placeholder="Guest"
+          onClick={onClickMod}
+        />
+        <SearchIcon className={styles.searchIcon} onClick={onClickMod} />
+      </div>
     </div>
   );
 };
 
 export default Header;
-
