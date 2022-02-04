5dbfbf05cf8ba8f7b159734e72281445d1f28736
Rebecca Mollica <rebecca.mollica@live.it>
2022-02-04 16:42:00 +0100

    fix: card description available beds

--- src/Components/CardDescription/index.jsx
+++ src/Components/CardDescription/index.jsx
@@ -12,7 +12,7 @@ const CardDescription = ({superHost, type, title, beds, rating }) => {
   return (
     <div className={styles.card_description}>
       <div className={styles.card__descriptionContainer}>
-        <span className={styles.info}>{superHost} {type} . {beds} beds</span>
+        <span className={styles.info}>{superHost} {type} {beds > 0 && <>. {beds} beds</>}</span>
         <div className={styles.ratingWrapper}>
           <span className={styles.rating}>⭐{rating}</span>
         </div>
